422166086a463c708dba5a544231cf9a6973a257
DJurecki <dejwux1@gmail.com>
2020-02-20 13:51:22 +0100

    Implement Interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/com/jurecki/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.jurecki.sfgpetclinic.services;
+
+import com.jurecki.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/jurecki/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.jurecki.sfgpetclinic.services;
+
+import com.jurecki.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
