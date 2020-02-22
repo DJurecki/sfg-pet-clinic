c0d723f6305b6bd4a78482b0ef0384cce62fb8ba
DJurecki <dejwux1@gmail.com>
2020-02-22 17:16:00 +0100

    Added specialties to vets on startup. Closes #34.

--- pet-clinic-data/src/main/java/com/jurecki/sfgpetclinic/services/SpecialtyService.java
+++ pet-clinic-data/src/main/java/com/jurecki/sfgpetclinic/services/SpecialtyService.java
@@ -2,5 +2,5 @@ package com.jurecki.sfgpetclinic.services;
 
 import com.jurecki.sfgpetclinic.model.Speciality;
 
-public interface SpecialtiesService extends CrudService<Speciality, Long> {
+public interface SpecialtyService extends CrudService<Speciality, Long> {
 }
