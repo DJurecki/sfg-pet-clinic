487da7e472490776677157a0d9b714e2c6d65e03
DJurecki <dejwux1@gmail.com>
2020-03-08 19:09:28 +0100

    Implemented find owner. Closes #54.

--- pet-clinic-data/src/main/java/com/jurecki/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/jurecki/sfgpetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package com.jurecki.sfgpetclinic.services;
 
 import com.jurecki.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
